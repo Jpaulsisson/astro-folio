ef20b6e5805cd5c99513c93ce48489d32fb36f69
Paul Sisson <paulsissonsemail@gmail.com>
2023-08-24 08:43:49 -0500

    Add modal and footer components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primaryFont': 'var(--primaryFont)',
+				'primaryBg': 'var(--primaryBg)',
+				'accentGreen': 'var(--accentGreen)',
+				'accentPink': 'var(--accentPink)',
+				'accentBlue': 'var(--accentBlue)',
+			},
+			maxWidth: {
+				'cutoff': '850px',
+			}
+		},
 	},
 	plugins: [],
 }
